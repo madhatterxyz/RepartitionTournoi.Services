64921c591cbc124852b6b6dfb2ca01e9bd185fbe
Nicolas <Nicolas@PC-NICOLAS>
2024-03-02 17:08:29 +0100

    burndown chart par joueur

--- Controllers/MatchsController.cs
+++ Controllers/MatchsController.cs
@@ -1,41 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
-using RepartitionTournoi.Domain;
 using RepartitionTournoi.Domain.Interfaces;
 using RepartitionTournoi.Models;
 using RepartitionTournoi.Models.Tournoi;
 
-namespace RepartitionTournoi.Services.Controllers
+namespace RepartitionTournoi.Services.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MatchsController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class MatchsController : ControllerBase
+    private readonly IMatchDomain _matchDomain;
+
+    public MatchsController(IMatchDomain matchDomain)
     {
+        _matchDomain = matchDomain;
+    }
 
-        private readonly ILogger<MatchsController> _logger;
-        private readonly IMatchDomain _matchDomain;
-
-        public MatchsController(ILogger<MatchsController> logger, IMatchDomain matchDomain)
-        {
-            _logger = logger;
-            _matchDomain = matchDomain;
-        }
-
-        [HttpGet]
-        [Route("BurndownChartLines/{id}")]
-        public IEnumerable<BurndownChartLineDTO> GetBurdownChartLines(int id)
-        {
-            return _matchDomain.GetBurndownChartLineDTOs(id);
-        }
-        [HttpGet]
-        [Route("{id}")]
-        public MatchDTO GetById(int id)
-        {
-            return _matchDomain.GetById(id);
-        }
-        [HttpPut]
-        public async Task Update([FromBody] MatchDTO match)
-        {
-            await _matchDomain.Update(match);
-        }
+    [HttpGet]
+    [Route("BurndownChartLines/{id}")]
+    public IEnumerable<BurndownChartLineDTO> GetBurdownChartLines(long id, long? joueurId)
+    {
+        return _matchDomain.GetBurndownChartLineDTOs(id, joueurId);
+    }
+    [HttpGet]
+    [Route("{matchId}/Tournoi")]
+    public TournoiDTO GetByMatchId(long matchId)
+    {
+        return _matchDomain.GetTournoiByMatchId(matchId);
+    }
+    [HttpGet]
+    [Route("{id}")]
+    public MatchDTO GetById(int id)
+    {
+        return _matchDomain.GetById(id);
+    }
+    [HttpPut]
+    public async Task Update([FromBody] MatchDTO match)
+    {
+        await _matchDomain.Update(match);
     }
 }
